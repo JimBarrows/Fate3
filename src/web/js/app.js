8ddf60cf62cfca2f22e78ff784f6b88f090c6158
Jim Barrows <Jim.Barrows@gmail.com[color]>
2013-12-07 00:31:28 -0700

    emberjs and new bootstrap fully installed

---
+++ src/web/js/app.js
@@ -0,0 +1,11 @@
+App = Ember.Application.create();
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+
+});
