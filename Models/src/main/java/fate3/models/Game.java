c85c81331ab755468332ba7cd69cd8629afcb0ef
Jim Barrows <Jim.Barrows@gmail.com>
2014-01-17 23:38:51 -0700

    More tweaks to make things better for persistence

--- Models/src/main/java/fate3/models/Game.java
+++ Models/src/main/java/fate3/models/Game.java
@@ -1,7 +1,6 @@
 package fate3.models;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +14,7 @@ import org.hibernate.validator.constraints.NotBlank;
  * Entity implementation class for Entity: Game
  * 
  */
-@Entity
+@Entity(name="game")
 public class Game implements Serializable {
 
 	@Id
@@ -36,11 +35,11 @@ public class Game implements Serializable {
 		super();
 	}
 
-	public UUID getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(UUID id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -74,6 +73,38 @@ public class Game implements Serializable {
 
 	public void setScale(String scale) {
 		this.scale = scale;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Game [id=%s, version=%s, name=%s, setting=%s, scale=%s]", id,
+				version, name, setting, scale);
 	}
 
 }
